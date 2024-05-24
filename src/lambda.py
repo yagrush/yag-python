9569e9b74b292e3863d180b6369a70d5b0ac0ed2
yagrush <yagrush@users.noreply.github.com>
2024-05-24 19:53:14 +0900

    test

--- src/lambda.py
+++ src/lambda.py
@@ -7,5 +7,8 @@ def get_func():
 
 print(get_func()(4))
 
-m = map(lambda x, y: x + y, ((999, 2), (4, 5)), ((3, 4), (6, 8)))
+m = map(lambda x, y: x + y, ((999, 2), (4, 5)), ((3, 4), (6, 8), (61111, 111118)))
 print(list(m))
+
+sample = [1, 2, 3, 4, 5]
+print(list(map(str, sample)))
